d9aea8cb6021c5c1883f9a2deb8b451f469d6d32
Allclassespatna1 <167908825+Allclassespatna1@users.noreply.github.com>
2024-05-30 17:12:56 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id = 21111175
-api_hash = os.environ.get("API_HASH", "fffea26a10b8c26ba900e1ed3d013d90")
+api_id = 21537501
+api_hash = os.environ.get("API_HASH", "7360337401:AAEuseHJwpcAbf9hRqebbVFxlUO9Y6o0NZg")
 bot_token = os.environ.get("BOT_TOKEN")
 auth_users = os.environ.get("AUTH_USERS", "6573766001")
 sudo_users = [int(num) for num in auth_users.split(",")]
